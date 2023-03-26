bd09cd13e6cc86c12672eb5692c7559e56f3a115
Jorge Pérez <jorperse@gmail.com>
2023-03-26 10:03:14 +0200

    Update Scan2Neo.py

--- Scan2Neo.py
+++ Scan2Neo.py
@@ -33,7 +33,7 @@ for folder, files in json_files.items():
             data = json.load(f)
 
             # Recorremos los nodos principales (direcciones IP)
-            for ip, ports in data.items():
+            for ip, ip_data in data.items():
                 # Merge or create the IP node
                 ip_node = Node("IP", address=ip)
                 existing_ip = graph.nodes.match("IP", address=ip).first()
@@ -41,11 +41,32 @@ for folder, files in json_files.items():
                     ip_node = existing_ip
                 else:
                     graph.create(ip_node)
+                    
+                # Create or merge the subnet nodes
+                if "subred" in ip_data:
+                    subnet = ip_data["subred"]
+                    subnet_node = Node("Subnet", address=subnet)
+                    existing_subnet = graph.nodes.match("Subnet", address=subnet).first()
+                    if existing_subnet:
+                        subnet_node = existing_subnet
+                    else:
+                        graph.create(subnet_node)
 
+                    # Creamos una relación entre el folder y la dirección IP
+                    folder_subnet_rel = Relationship(folder_node, "HAS_SUBNET", subnet_node)
+                    graph.create(folder_subnet_rel)
+                    
+                    # Creamos una relación entre el folder y la dirección IP
+                    folder_ip_rel = Relationship(subnet_node, "HAS_IP", ip_node)
+                    graph.create(folder_ip_rel)
+                else:
+                    # Creamos una relación entre el folder y la dirección IP
+                    folder_ip_rel = Relationship(folder_node, "HAS_IP", ip_node)
+                    graph.create(folder_ip_rel)
+                        
                 # Create or merge the port nodes
-                for port_number, port_data in ports.items():
+                for port_number, port_data in ip_data["ports"].items():
                     existing_port = graph.nodes.match("Port", number=port_number, date=date_str).first()
-
                     if existing_port is not None:
                         # Check if the attributes of the existing port are the same as the new port
                         if all(existing_port.get(key) == value for key, value in port_data.items()):
@@ -59,10 +80,6 @@ for folder, files in json_files.items():
                         # If there is no existing port, create a new port node
                         port_node = Node("Port", number=port_number, date=date_str, **port_data)
                         graph.create(port_node)
-
-                    # Creamos una relación entre el folder y la dirección IP
-                    folder_ip_rel = Relationship(folder_node, "HAS_IP", ip_node)
-                    graph.create(folder_ip_rel)
                     
                     # Creamos una relación entre la dirección IP y el puerto
                     ip_port_rel = Relationship(ip_node, "HAS_PORT", port_node)
